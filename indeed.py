03d8db269b72d4989fc12291f87e9061944487d3
dinasinclair <admin@RainyDays-MacBook-Pro.local>
2020-05-18 22:44:02 -0700

    need to fix pagination

--- indeed.py
+++ indeed.py
@@ -27,9 +27,12 @@ APPLY_NB = 'https://jobs.lever.co/innovateschools/c142ef0c-c426-4c8d-b06c-eb6720
 @dataclass_json
 @dataclass
 class JobInfo:
+    app_type: str
     app_text: str
-    url: str = None  # TODO: make not none, collect during get()
-    app_type: str = None
+    indeed_url: str
+    company_url: str = None
+    page_number: int = None
+    rank: int = None
     description: str = None  # TODO: make not none, collect during get()
     title: str = None  # TODO: make not none, collect during get()
     stats: Dict = None
@@ -48,6 +51,7 @@ class SearchScraper:
 
     def __init__(self):
         self.driver = webdriver.Chrome()
+        self.pagination_limit = 2
 
     def print_all_iframes(self) -> None:
         """Prints all iframes in a current driver page source.
@@ -74,25 +78,42 @@ class SearchScraper:
             # if 'apply' in ii.get_attribute('id'):
             print(ii.get_attribute('id'))
 
-    def get_apply_now_text(self):
+    def get_apply_now_text(self) -> JobInfo:
+        """
+        Gets information from an apply now job.
+        Assumes driver is already at an apply now job url.
+        Returns: JobInfo object with job details.
+        """
         # Find apply button for apply-now scenario
+        indeed_url = self.driver.current_url
         apply_button = self.driver.find_element_by_id('indeedApplyButtonContainer')
 
         # Click on apply button for apply-now
         apply_button.click()
-        self.driver.implicitly_wait(2)
+        self.driver.implicitly_wait(1)
         iframe = self.driver.find_element_by_xpath("//iframe[@title='No content']")
         self.driver.switch_to.frame(iframe)
-        self.driver.implicitly_wait(2)
+        self.driver.implicitly_wait(1)
         iframe_apply = self.driver.find_element_by_xpath("//iframe[@title='Apply Now']")
         self.driver.switch_to.frame(iframe_apply)
-        self.driver.implicitly_wait(2)
-        return JobInfo.from_dict({'app_text': self.strip_html(self.driver.page_source)})
+        self.driver.implicitly_wait(1)
 
-    def get_company_site_text(self):
+        # Create and return JobInfo
+        return JobInfo.from_dict({'app_text': self.strip_html(self.driver.page_source),
+                                  'app_type': 'apply_now',
+                                  'indeed_url': indeed_url})
+
+    def get_company_site_text(self) -> JobInfo:
+        """
+        Gets information from a company site application.
+        Assumes driver is already at the indeed job posting url.
+        Returns: JobInfo object with job details.
+        """
         # Click on apply button for on company site
+        indeed_url = self.driver.current_url
         link_to_site = self.driver.find_element_by_xpath('//a[text()="Apply On Company Site"]')
         self.driver.get(link_to_site.get_attribute('href'))
+        self.driver.implicitly_wait(5)
         print("company site url: ", self.driver.current_url)
 
         # Process all lever jobs
@@ -101,12 +122,18 @@ class SearchScraper:
             # First page is description for lever forms, second page is app questions
             link_to_app = self.driver.find_element_by_xpath('//a[text()="Apply for this job"]')
             self.driver.get(link_to_app.get_attribute('href'))
-            return JobInfo.from_dict({'app_text': self.strip_html(self.driver.page_source)})
+            return JobInfo.from_dict({'app_text': self.strip_html(self.driver.page_source),
+                                      'app_type': 'jobs.lever',
+                                      'indeed_url': indeed_url,
+                                      'company_url': self.driver.current_url})
         # Process all greenhouse jobs
         if self.driver.current_url.startswith("https://boards.greenhouse.io/"):
             print("This is a boards.greenhouse application")
             # greenhouse jobs put their info on the initial page, no click through needed!
-            return JobInfo.from_dict({'app_text': self.strip_html(self.driver.page_source)})
+            return JobInfo.from_dict({'app_text': self.strip_html(self.driver.page_source),
+                                      'app_type': 'boards.greenhouse',
+                                      'indeed_url': indeed_url,
+                                      'company_url': self.driver.current_url})
         # TODO: fix case that job lever goes straight to app page like https://jobs.lever.co/amobee/d049864f-6949-4162-80fa-64ab217bd96b/apply?lever-source=Glassdoor
         # TODO: add jobs.jobvite? ProbablyMonsters is one ex that uses, also https://jobs.jobvite.com/the-climate-corporation/job/ojwP9fwx?__jvst=Job+Board&__jvsd=Indeed
         # TODO: https://chj.tbe.taleo.net/? zonar uses, also https://dtt.taleo.net/careersection/10260/jobdetail.ftl?lang=en&job=E20NATCSRCVS022-SA&src=JB-16801
@@ -119,16 +146,19 @@ class SearchScraper:
         # TODO: add https://jobs.smartrecruiters.com/oneclick-ui/company/103157783/job/1609939482/publication/743999711924474
         else:
             print("Can't process this app :/")
-            return JobInfo.from_dict({'app_text': 'Cannot process this app :('})
+            return JobInfo.from_dict({'app_text': "Can't Process",
+                                      'app_type': 'unknown',
+                                      'indeed_url': indeed_url,
+                                      'company_url': self.driver.current_url})
 
     @staticmethod
-    def strip_html(source_page):
+    def strip_html(source_page: str) -> str:
+        # Get BS from page source, convert to lowercase text blob
         page_soup = BeautifulSoup(source_page, "html.parser")
         return page_soup.get_text("\t", strip=True).lower()
 
     @staticmethod
-    def html_to_stats(html_text, verbose=False):
-        # Get BS from page source, convert to lowercase text blob
+    def html_to_stats(html_text: str, verbose: bool = False) -> Dict:
         stats_dict = {}
         app_text = SearchScraper.strip_html(html_text)
 
@@ -162,8 +192,8 @@ class SearchScraper:
 
         return job_links
 
-    def process_job_url(self, job_page_url):
-        self.driver.get(job_page_url)
+    def process_job_url(self, job_url: str) -> JobInfo:
+        self.driver.get(job_url)
         # Check if this is an apply-now job
         is_apply_now = len(self.driver.find_elements_by_id('indeedApplyButtonContainer')) == 1
 
@@ -171,40 +201,67 @@ class SearchScraper:
             print("This is an apply now job!")
             job_info = self.get_apply_now_text()
 
-
         else:
             print("This is an external application!")
             job_info = self.get_company_site_text()
 
-        job_info.app_type = is_apply_now
         job_info.stats = self.html_to_stats(job_info.app_text)
         print("STATS: ", job_info.stats)
         return job_info
 
-    def next_page_exists(self):
+    # TODO make this next page work!!
+    def next_page_exists(self) -> bool:
         """
-
-        Returns:
-
+        Returns: bool, true if there is a next page button.
         """
-        pagination = self.driver.find_elements_by_class_name('np')
-        if len(pagination) == 1:
-            if "Next" in pagination[0].text:
-                return True
-            else:
-                return False
+        # pagination = self.driver.find_elements_by_class_name('pagination-list')
+        # pagination = self.driver.find_elements_by_xpath("//aria-label")
+        # pagination = self.driver.find_element_by_xpath("//div*[@aria-label='Next']")
+
+        pagination = self.driver.find_elements_by_xpath('//*[@aria-label]')
+
+        print (pagination)
+        print(len(pagination))
         if len(pagination) == 0:
             return False
-        if len(pagination) > 1:
-            assert ValueError("Only expect one pagination (np) item per chrome page.")
+        else:
+            for elem in pagination:
+                aria_label = elem.get_attribute("aria-label")
+                print("aria: ", aria_label)
+                if aria_label is not None and "Next" in aria_label:
+                    return True
+        return False
 
-    def get_next_page(self):
+        # if len(pagination) < 2:
+        #     aria_label = pagination[-1].find_element_by_css_selector('a').get_attribute("aria-label")
+        #     print ("aria: ", aria_label)
+        #     if aria_label is not None and "Next" in aria_label:
+        #         return True
+        #     else:
+        #         return False
+        # if len(pagination) > 1:
+        #     assert ValueError("Only expect one pagination (np) item per chrome page.")
+
+    def get_next_page(self) -> None:
+        """
+        Navigates to next page of indeed job postings.
+        Returns: None
+        """
         try:
             next_page_button = self.driver.find_element_by_class_name('np')
         except:
             print("Expected exactly one Next>> elem. Did you validate that next page exists?")
         else:
             next_page_button.click()
+
+    def get_job_normalized_json(self, job_link: str, page_number: int, rank: int):
+        job_url = 'http://indeed.com/' + job_link['href']
+        print("job page url: ", job_url)
+        job_info = self.process_job_url(job_url)
+        job_info.page_number = page_number
+        job_info.rank = rank
+        normalized_json = pd.json_normalize([job_info.to_dict()])
+        return normalized_json
 
     def run(self, search_url):
         """
@@ -215,48 +272,62 @@ class SearchScraper:
         Returns:
 
         """
-        # Connect to the search result URL
+        # Connect to the search result URL, init page number
         self.driver.get(search_url)
+        print("Next page exists? ", self.next_page_exists())
+        page_number = 1
+        rank = 1
 
         # Parse HTML and save to BeautifulSoup object
         soup = BeautifulSoup(self.driver.page_source, "html.parser")
 
-        # Grabbing first job link as an example
+        # Grabbing all job links
         job_links = self.get_all_job_links(soup)
 
         # Get and connect to the first job page URL for df col format
-        job_url = 'http://indeed.com/' + job_links[0]['href']
-        print("job page url: ", job_url)
-
-        # Scrape the application
-        job_info = self.process_job_url(job_url)
-        job_info.url = job_url
-        normalized_json = pd.json_normalize([job_info.to_dict()])
-
-        # Note: passing index 0 okay because we're creating a one row df
+        normalized_json = self.get_job_normalized_json(job_links[0], page_number, rank)
         df = pd.DataFrame(data=normalized_json, index=[0])
         self.driver.implicitly_wait(5)
+        print("Processed job {}!".format(rank))
+        rank += 1
+
 
         for job in job_links[1:]:
             # Get and connect to the job page URL
-            job_url = 'http://indeed.com/' + job['href']
-            print("job page url: ", job_url)
-
-            # Scrape the application
-            job_info = self.process_job_url(job_url)
-            job_info.url = job_url
-            normalized_json = pd.json_normalize([job_info.to_dict()])
-
+            normalized_json = self.get_job_normalized_json(job, page_number, rank)
             # Note: passing index 0 okay because we're creating a one row df
             df = df.append(pd.DataFrame(data=normalized_json, index=[0]))
             self.driver.implicitly_wait(5)
+            print("Processed job {}!".format(rank))
+            rank += 1
+        page_number += 1
+
+        print("about to paginate!")
+        print("Next page exists? ", self.next_page_exists())
+        print
+
+        # Repeat as long as there's another page to grab
+        while self.next_page_exists() and page_number <= self.pagination_limit:
+            print("On page {}!".format(page_number))
+            self.get_next_page()
+            soup = BeautifulSoup(self.driver.page_source, "html.parser")
+            job_links = self.get_all_job_links(soup)
+            for job in job_links:
+                # Get and connect to the job page URL
+                normalized_json = self.get_job_normalized_json(job, page_number, rank)
+                # Note: passing index 0 okay because we're creating a one row df
+                df = df.append(pd.DataFrame(data=normalized_json, index=[0]))
+                self.driver.implicitly_wait(5)
+                print("Processed job {}!".format(rank))
+                rank += 1
+            page_number += 1
 
         df.to_csv('job_data.csv')
         print("Done!! :D")
 
 
 if __name__ == "__main__":
-    search_url = APPLY_ON_COMPANY_SITE_CONVOY_URL
+    search_url = ALL_MLE_SEA_URL
     search_scraper = SearchScraper()
 
     # Test that main data creation for one page works
